4a4b0a13c86451eb1cd0e0451aacab9e9bf61971
Balazs P <balazs.p@rankmath.com>
2024-07-19 00:58:54 +0200

    Bump version in constant too

--- wp-autoplugin.php
+++ wp-autoplugin.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define constants
-define( 'WP_AUTOPLUGIN_VERSION', '1.0' );
+define( 'WP_AUTOPLUGIN_VERSION', '1.0.1' );
 define( 'WP_AUTOPLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WP_AUTOPLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
